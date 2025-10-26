3c7f97deede28dba4dbf9ebbd9d4357ed8311ce9
Fazo01 <fg7829098@gmail.com>
2025-10-26 22:25:50 +0500

    Initialize to mongodb favourite in practicing lecture 16

--- Practicing Lecture-16/models/favourite.js
+++ Practicing Lecture-16/models/favourite.js
@@ -1,25 +1,16 @@
-
+const { getDB } = require('../utils/databaseUtils')
 
 module.exports=class Favourite{
   static addToFavourite(homeId,callback){
-    Favourite.getFavourite((favourites)=>{
-      if(favourites.includes(homeId)){
-        callback("Home already in favourite")
-      }else{
-        favourites.push(homeId)
-        fs.writeFile(favouritepath,JSON.stringify(favourites),callback)
-      }
-    })
+    
   }
   static getFavourite(callback){
-    fs.readFile(favouritepath,(err,data)=>{
-      callback(!err?JSON.parse(data):[])
-    })
+    
   }
-  static deleteById(delHomeId,callback){
-    Favourite.getFavourite(homeIds=>{
-      homeIds=homeIds.filter(homeId=>delHomeId!==homeId)
-      fs.writeFile(favouritepath,JSON.stringify(homeIds),callback)
-    })
+  
+  static deleteById(homeId){
+    console.log(homeId)
+        const db = getDB()
+        return db.collection('homes').deleteOne({ _id: new ObjectId(String(homeId)) })
   }
 }
