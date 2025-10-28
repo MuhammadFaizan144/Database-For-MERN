f556bc74bee6d6fe5cd6cf88ccc29be5b72235ad
Fazo01 <fg7829098@gmail.com>
2025-10-28 17:14:25 +0500

    complete create home schema in Practicing lecture 17

---
+++ Practicing Lecture-17/models/home.js
@@ -0,0 +1,26 @@
+const { ObjectId } = require('mongodb')
+const mongoose=require('mongoose')
+const homeSchema=mongoose.Schema({
+  houseName:{type:String, required:true},
+  price:{type:String, required:true},
+  location:{type:String, required:true},
+  rating:String,
+  photoURL:String,
+});
+// module.exports = class Home {
+//   constructor(houseName, price, location, rating, photoURL, _id) {
+//     this.houseName = houseName
+//     this.price = price
+//     this.location = location
+//     this.rating = rating
+//     this.photoURL = photoURL
+
+//       this._id = _id
+// save() 
+// static fetchAll()
+// static findById(homeId)
+// static deleteById(homeId)
+
+//     
+
+ 
