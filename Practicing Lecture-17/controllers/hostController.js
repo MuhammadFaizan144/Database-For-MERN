28bf6edf6f754c36a62e4b810a7ccd31b1200c95
Fazo01 <fg7829098@gmail.com>
2025-10-29 17:16:38 +0500

    Complete deleting home in Practicing Lecture 17

--- Practicing Lecture-17/controllers/hostController.js
+++ Practicing Lecture-17/controllers/hostController.js
@@ -60,11 +60,10 @@ exports.postEditHome = (req, res, next) => {
 exports.postDeleteHome = (req, res, next) => {
   const homeId = req.params.homeId
   console.log("Came to delete home ID", homeId)
-  Home.deleteById(homeId, error => {
-    if (error) {
-      console.log("Error while deleting", error)
-    }
+  Home.findByIdAndDelete(homeId).then(()=>{
     res.redirect("/host/host-home-list")
+  }).catch((error)=>{
+    console.log('Error while deleting ',error)
   })
 }
 exports.registeredHome = registeredHome
