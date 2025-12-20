5c41f915500123590235a2f675e692bead9b9134
Fazo01 <fg7829098@gmail.com>
2025-12-20 10:22:42 +0500

    Complete Setting Up complete Practicing lecture 17

--- Practicing Lecture-18/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const authRouter=express.Router()
-
-const authController=require("../controllers/authController")
-authRouter.get("/login",authController.getLogin)
-authRouter.post("/login",authController.postLogin)
-authRouter.post("/logout",authController.postLogout)
-module.exports=authRouter
